f5c5acd1865e5ac9b64e4551c56855c7ab0fe9fc
Cecilia Wong <ceciliakhwong@gmail.com>
2017-11-13 21:23:11 -0500

    Add/remove dark and light class for body & status text depending on switch on/off

--- js/main.js
+++ js/main.js
@@ -1,13 +1,24 @@
 document.addEventListener('DOMContentLoaded', function() {
   var lightSwitch = document.querySelector('.switch');
+  var status = document.querySelector('.status');
 
   lightSwitch.addEventListener('click', function() {
     if (this.classList.contains('off')) {
       this.classList.remove('off');
       this.classList.add('on');
+
+      document.body.classList.remove('dark');
+      document.body.classList.add('light');
+
+      status.innerText = "It's so bright in here!";
     } else {
       this.classList.remove('on');
       this.classList.add('off');
+
+      document.body.classList.remove('light');
+      document.body.classList.add('dark');
+
+      status.innerText = "It's so dark in here!";
     }
   })
 
