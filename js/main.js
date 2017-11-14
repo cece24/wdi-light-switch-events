29910daec57bedce502f68b719af42a68a4d8730
Cecilia Wong <ceciliakhwong@gmail.com>
2017-11-13 21:03:30 -0500

    Select button and add event listener to add/remove classes upon click

--- js/main.js
+++ js/main.js
@@ -1,2 +1,14 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var lightSwitch = document.querySelector('.switch');
 
+  lightSwitch.addEventListener('click', function() {
+    if (this.classList.contains('off')) {
+      this.classList.remove('off');
+      this.classList.add('on');
+    } else {
+      this.classList.remove('on');
+      this.classList.add('off');
+    }
+  })
 
+});
